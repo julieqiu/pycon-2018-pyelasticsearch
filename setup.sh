677d0e48f9aeab9f4c39b0f78b2ccb51a3609f3a
Julie Qiu <julieyeqiu@gmail.com>
2018-04-14 10:02:44 -0400

    add FLASK_DEBUG=1

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@ source venv/bin/activate
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export FLASK_APP=${DIR}/searchapp/searchapp.py
+export FLASK_DEBUG=1
 
 brew services start elasticsearch

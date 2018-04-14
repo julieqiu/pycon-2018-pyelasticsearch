cd023aab29e1baf2e848a8c732ba80ae932b5fc1
Jim Grandpre <jimtla@gmail.com>
2018-04-14 08:15:05 -0400

    Reasonable styling

--- searchapp/app/routes.py
+++ searchapp/app/routes.py
@@ -3,13 +3,13 @@ from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search
 from app import app
 
-def search(term):
+def search(term, count):
     client = Elasticsearch()
     client.transport.connection_pool.connection.headers.update({'content-type':
         'application/json'})
 
     s = Search(using=client, index='products', doc_type='products')
-    docs = s.query('term', name=term).execute()
+    docs = s.query('term', name=term)[0:count].execute()
 
     return docs
 
@@ -17,10 +17,9 @@ def search(term):
 @app.route('/')
 @app.route('/index')
 def index():
-    terms = [ 'Bonobos Pants', 'shirt', 'pants' , 'dress', 'jacket', 'a',
-            'blazer']
+    terms = ['shirt', 'pants', 'dress', 'jacket', 'a', 'blazer']
 
-    products_by_category = {t: search(t) for t in terms}
+    products_by_category = {t: search(t, 9) for t in terms}
 
     return render_template(
         'index.html',

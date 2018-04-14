ab905e44e7f97540716d9ed6a570bc02f8b5bba0
Julie Qiu <julieyeqiu@gmail.com>
2018-04-14 11:39:34 -0400

    added pip install -e . to setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export FLASK_APP=${DIR}/searchapp/searchapp.py
 export FLASK_DEBUG=1
 
+pip install -e .
+
 brew services start elasticsearch
 brew services start kibana

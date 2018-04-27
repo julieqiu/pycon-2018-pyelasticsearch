58b077aac5de03139bff65b371b1d8f7eba68306
Jim Grandpre <jimtla@gmail.com>
2018-04-26 21:28:02 -0400

    Necklce

--- searchapp/app/search.py
+++ searchapp/app/search.py
@@ -13,7 +13,7 @@ def search(term: str, count: int) -> dict:
     client.transport.connection_pool.connection.headers.update(HEADERS)
 
     s = Search(using=client, index=INDEX_NAME, doc_type=DOC_TYPE)
-    name_query = Q('match', name=dict(query=term, operator='and'))
+    name_query = Q('match', name=dict(query=term, operator='and', fuzziness='2'))
     docs = s.query(name_query).execute()
 
     return docs
